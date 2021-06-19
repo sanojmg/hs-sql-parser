f77414072066ca3042287209b91a11f590d5e4d9
Sanoj MG <sanoj.george.dev@gmail.com>
2021-06-19 23:01:11 +1000

    Disabled pretty printing for testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,76 +1,79 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Semigroup ((<>))
-import Options.Applicative
-import System.IO
-import qualified Data.Text as T  
-import qualified Data.Text.IO as TIO 
-import qualified Text.Parsec as P (ParseError) 
+-- import Data.Semigroup ((<>))
+-- import Options.Applicative
+-- import System.IO
+-- import qualified Data.Text as T  
+-- import qualified Data.Text.IO as TIO 
+-- import qualified Text.Parsec as P (ParseError) 
 
-import Prettyprinter
-import Prettyprinter.Render.Text
+-- import Prettyprinter
+-- import Prettyprinter.Render.Text
 
-import Parser
-import Pretty
-import Type
+-- import Parser
+-- import Pretty
+-- import Type
 
-data Conf = Conf { inputFile :: Maybe FilePath,
-                   outputFile :: Maybe FilePath,
-                   prettyPrint :: Bool
-            }
-             deriving Show
+-- data Conf = Conf { inputFile :: Maybe FilePath,
+--                    outputFile :: Maybe FilePath,
+--                    prettyPrint :: Bool
+--             }
+--              deriving Show
 
-conf :: Parser Conf
-conf = Conf 
-    <$> (optional $ strOption 
-         ( long "input-file"
-         <> short 'i'
-         <> metavar "INPUTFILE"
-         <> help "Input File" ))
-    <*> (optional $ strOption 
-         ( long "output-file"
-         <> short 'o'
-         <> metavar "OUTPUTFILE"
-         <> help "Output File" ))
-    <*> switch
-         ( long "pretty"
-         <> short 'p'
-         <> help "Whether to pretty print the AST" )
+-- conf :: Parser Conf
+-- conf = Conf 
+--     <$> (optional $ strOption 
+--          ( long "input-file"
+--          <> short 'i'
+--          <> metavar "INPUTFILE"
+--          <> help "Input File" ))
+--     <*> (optional $ strOption 
+--          ( long "output-file"
+--          <> short 'o'
+--          <> metavar "OUTPUTFILE"
+--          <> help "Output File" ))
+--     <*> switch
+--          ( long "pretty"
+--          <> short 'p'
+--          <> help "Whether to pretty print the AST" )
  
-opts :: ParserInfo Conf 
-opts = info (conf <**> helper)
-    ( fullDesc 
-     <> progDesc "Parse the SQL query and pretty print the AST"
-     <> header "hs-sql-parser: A parser in Haskell for simple SQL like queries")
+-- opts :: ParserInfo Conf 
+-- opts = info (conf <**> helper)
+--     ( fullDesc 
+--      <> progDesc "Parse the SQL query and pretty print the AST"
+--      <> header "hs-sql-parser: A parser in Haskell for simple SQL like queries")
 
 main :: IO ()
-main = do
-    config <- execParser opts
-    parseSQL config 
+main = print "Undefined"
 
-parseSQL :: Conf -> IO ()
-parseSQL conf = do
-    sqlStr <- readInFile $ inputFile conf
-    let ast = runParser $ T.unpack sqlStr
-    writeAST ast (outputFile conf) (prettyPrint conf)
+-- main :: IO ()
+-- main = do
+--     config <- execParser opts
+--     parseSQL config 
+
+-- parseSQL :: Conf -> IO ()
+-- parseSQL conf = do
+--     sqlStr <- readInFile $ inputFile conf
+--     let ast = runParser $ T.unpack sqlStr
+--     writeAST ast (outputFile conf) (prettyPrint conf)
     
-readInFile (Just fileName) = TIO.readFile fileName
-readInFile Nothing = TIO.getContents
+-- readInFile (Just fileName) = TIO.readFile fileName
+-- readInFile Nothing = TIO.getContents
 
-writeAST :: Either P.ParseError SelectStmnt -> Maybe FilePath -> Bool -> IO ()
-writeAST (Left err) _ _ = print $ "Error parsing SQL: " <> show err
-writeAST (Right sel) outFile isPretty = 
-    if isPretty then
-        writeDocOutFile outFile (pretty sel)
-    else
-        writeOutFile outFile (T.pack $ show sel)
+-- writeAST :: Either P.ParseError SelectStmnt -> Maybe FilePath -> Bool -> IO ()
+-- writeAST (Left err) _ _ = print $ "Error parsing SQL: " <> show err
+-- writeAST (Right sel) outFile isPretty = 
+--     if isPretty then
+--         writeDocOutFile outFile (pretty sel)
+--     else
+--         writeOutFile outFile (T.pack $ show sel)
     
-writeOutFile (Just fileName) str = TIO.writeFile fileName str
-writeOutFile Nothing str = TIO.putStr str   
+-- writeOutFile (Just fileName) str = TIO.writeFile fileName str
+-- writeOutFile Nothing str = TIO.putStr str   
 
-writeDocOutFile Nothing doc = hPutDoc stdout doc 
-writeDocOutFile (Just fileName) doc = withFile fileName WriteMode act
-    where
-        act h = hPutDoc h doc
+-- writeDocOutFile Nothing doc = hPutDoc stdout doc 
+-- writeDocOutFile (Just fileName) doc = withFile fileName WriteMode act
+--     where
+--         act h = hPutDoc h doc
  
--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,136 +1,136 @@
 module Pretty where
 
-import qualified Data.Text as T
+-- import qualified Data.Text as T
 
-import Prettyprinter
-import Prettyprinter.Util
+-- import Prettyprinter
+-- import Prettyprinter.Util
 
-import Type
-import Parser (tparseEof)
-import Text.Parsec.String (Parser)
+-- import Type
+-- import Parser (tparseEof)
+-- import Text.Parsec.String (Parser)
 
--- class Pretty a where
---     pretty a :: a -> Doc
+-- -- class Pretty a where
+-- --     pretty a :: a -> Doc
 
-pprint :: Pretty a => Parser a -> String -> Doc ann
-pprint p s = case (tparseEof p s) of
-    Right t -> pretty t
-    Left e -> pretty $ "Error parsing: " ++ show e
+-- pprint :: Pretty a => Parser a -> String -> Doc ann
+-- pprint p s = case (tparseEof p s) of
+--     Right t -> pretty t
+--     Left e -> pretty $ "Error parsing: " ++ show e
 
 
-instance Pretty SelectStmnt where
-    pretty (SelectStmnt selectCl fromCl whereCl groupByCl orderByCl) = 
-            align $ vsep $ 
-                filter nonEmpty [pSel selectCl, 
-                                 pFrm fromCl, 
-                                 pWhr whereCl, 
-                                 pGrp groupByCl, 
-                                 pOrd orderByCl]
+-- instance Pretty SelectStmnt where
+--     pretty (SelectStmnt selectCl fromCl whereCl groupByCl orderByCl) = 
+--             align $ vsep $ 
+--                 filter nonEmpty [pSel selectCl, 
+--                                  pFrm fromCl, 
+--                                  pWhr whereCl, 
+--                                  pGrp groupByCl, 
+--                                  pOrd orderByCl]
 
-        where
-            nonEmpty :: Doc ann -> Bool
-            nonEmpty doc = (length $ show doc) > 0
+--         where
+--             nonEmpty :: Doc ann -> Bool
+--             nonEmpty doc = (length $ show doc) > 0
 
-            -- pSel :: [(Expr, Maybe T.Text)] -> Doc ann
-            pSel cols = align $ vsep [prettyS "SELECT", pCols cols]
+--             -- pSel :: [(Expr, Maybe T.Text)] -> Doc ann
+--             pSel cols = align $ vsep [prettyS "SELECT", pCols cols]
 
-            -- pCols :: [(Expr, Maybe T.Text)] -> Doc ann
-            pCols cols = indent 4 $ encloseSep emptyDoc emptyDoc comma (pCol <$> cols)
+--             -- pCols :: [(Expr, Maybe T.Text)] -> Doc ann
+--             pCols cols = indent 4 $ encloseSep emptyDoc emptyDoc comma (pCol <$> cols)
 
-            pCol (col, alias) = pretty col <+> pAl alias
-            pAl (Just an) = prettyS "AS " <+> prettyS an 
-            pAl Nothing = prettyS ""
+--             pCol (col, alias) = pretty col <+> pAl alias
+--             pAl (Just an) = prettyS "AS " <+> prettyS an 
+--             pAl Nothing = prettyS ""
             
-            pFrm :: [FromItem] -> Doc ann
-            pFrm frms = align $ prettyS "FROM " <+> vsep (pretty <$> frms)
+--             pFrm :: [FromItem] -> Doc ann
+--             pFrm frms = align $ prettyS "FROM " <+> vsep (pretty <$> frms)
 
-            pWhr :: Maybe WherePredicate -> Doc ann
-            pWhr (Just whr) = prettyS "WHERE " <+> (nest 2 $ pretty whr)
-            pWhr Nothing = emptyDoc
+--             pWhr :: Maybe WherePredicate -> Doc ann
+--             pWhr (Just whr) = prettyS "WHERE " <+> (nest 2 $ pretty whr)
+--             pWhr Nothing = emptyDoc
 
-            pGrp :: ColumnList -> Doc ann
-            pGrp [] = emptyDoc
-            pGrp cols@(x : xs) = prettyS "GROUP BY " <+> (align $ 
-                                    encloseSep emptyDoc emptyDoc comma (pretty <$> cols)) 
+--             pGrp :: ColumnList -> Doc ann
+--             pGrp [] = emptyDoc
+--             pGrp cols@(x : xs) = prettyS "GROUP BY " <+> (align $ 
+--                                     encloseSep emptyDoc emptyDoc comma (pretty <$> cols)) 
 
-            pOrd :: ColumnList -> Doc ann
-            pOrd [] = emptyDoc
-            pOrd cols@(x : xs) = prettyS "ORDER BY " <+> (align $ 
-                                    encloseSep emptyDoc emptyDoc comma (pretty <$> cols))   
-
-
-instance Pretty FromItem where
-    pretty (FromItemJoin joinReln) = pretty joinReln
-    pretty (FromItemNonJoin njReln) = pretty njReln
-
-instance Pretty NonJoinRelation where
-    pretty (TableRelation tn) = prettyS tn
-    pretty (Subquery sel) = parens $ (nest 4 $ line <> pretty sel) <> line 
-    pretty (FromAlias nj an) = pretty nj <+> pretty ("AS " ++ an)
-
-instance Pretty JoinRelation where
-    pretty (Join jType njReln jCrit) = pretty jType <+> pretty njReln <+> pretty jCrit
-
-instance Pretty JoinType where
-    pretty InnerJoin = prettyS "INNER JOIN" 
-    pretty LeftOuterJoin = prettyS "LEFT OUTER JOIN"  
-    pretty RightOuterJoin = prettyS "RIGHT OUTER JOIN"  
-    pretty FullOuterJoin = prettyS "FULL OUTER JOIN" 
-
-instance Pretty JoinCriteria where
-    pretty (JoinOn expr) = line <> prettyS "ON" <+> (nest 2 $ pretty expr)  
-
-instance Pretty Expr where
-    pretty (IntegralLit n) = pretty n
-    pretty (FractionalLit d) = pretty d
-    pretty (StringLit s) = squotes $ pretty s
-    pretty (Identifier s) = pretty s
-    pretty (QfdIdentifier t c) = pretty t <> dot <> pretty c
-    pretty Asterisk = prettyS "*"
-    pretty (QfdAsterisk t) = pretty t <> dot <> prettyS "*"
-    pretty (Neg e) = prettyS "-" <+> pretty e
-    pretty (ArithBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
-    pretty (Parens e) = parens $ pretty e
-    pretty (CaseSt ce) = pretty ce
-    pretty (Func f el) = pretty f <> (parens $ fillSep $ punctuate comma $ map pretty el) 
-    pretty NullLit = prettyS "NULL"
-    pretty (BoolLit b) = pretty b
-    pretty (Not e) = prettyS "NOT" <+> pretty e
-    pretty (Exists e) = prettyS "EXISTS" <+> pretty e
-    pretty (BoolBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
-    pretty (RelBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
+--             pOrd :: ColumnList -> Doc ann
+--             pOrd [] = emptyDoc
+--             pOrd cols@(x : xs) = prettyS "ORDER BY " <+> (align $ 
+--                                     encloseSep emptyDoc emptyDoc comma (pretty <$> cols))   
 
 
-instance Pretty CaseExpr where
-    pretty (CaseExpr w e) = align $ vsep [prettyS "CASE",
-                                          indent 4 $ vsep $ map wCl w,
-                                          indent 4 $ eCl e,
-                                          prettyS "END"]  
+-- instance Pretty FromItem where
+--     pretty (FromItemJoin joinReln) = pretty joinReln
+--     pretty (FromItemNonJoin njReln) = pretty njReln
 
-        where
-            wCl (wp, we) = fillSep [prettyS "WHEN", pretty wp, prettyS "THEN", pretty we]
-            eCl (Just expr) = prettyS "ELSE" <+> pretty expr
-            eCl Nothing = prettyS ""
+-- instance Pretty NonJoinRelation where
+--     pretty (TableRelation tn) = prettyS tn
+--     pretty (Subquery sel) = parens $ (nest 4 $ line <> pretty sel) <> line 
+--     pretty (FromAlias nj an) = pretty nj <+> pretty ("AS " ++ an)
 
-instance Pretty ArithBinOp where
-    pretty Add = prettyS "+"
-    pretty Subtract = prettyS "-"
-    pretty Multiply = prettyS "*"
-    pretty Divide = prettyS "/"
-    pretty Modulo = prettyS "%"
+-- instance Pretty JoinRelation where
+--     pretty (Join jType njReln jCrit) = pretty jType <+> pretty njReln <+> pretty jCrit
 
-instance Pretty RelnBinOp where
-    pretty LessOrEq = prettyS "<="
-    pretty GreaterOrEq = prettyS ">="
-    pretty Less = prettyS "<"
-    pretty Greater = prettyS ">" 
-    pretty Like = prettyS "LIKE"
-    pretty NotEq = prettyS "!="
-    pretty Eq = prettyS "="
+-- instance Pretty JoinType where
+--     pretty InnerJoin = prettyS "INNER JOIN" 
+--     pretty LeftOuterJoin = prettyS "LEFT OUTER JOIN"  
+--     pretty RightOuterJoin = prettyS "RIGHT OUTER JOIN"  
+--     pretty FullOuterJoin = prettyS "FULL OUTER JOIN" 
 
-instance Pretty BoolBinOp where
-    pretty And = line <> prettyS "AND"
-    pretty Or = line <> prettyS "OR"
+-- instance Pretty JoinCriteria where
+--     pretty (JoinOn expr) = line <> prettyS "ON" <+> (nest 2 $ pretty expr)  
 
-prettyS s = pretty (s :: String)
+-- instance Pretty Expr where
+--     pretty (IntegralLit n) = pretty n
+--     pretty (FractionalLit d) = pretty d
+--     pretty (StringLit s) = squotes $ pretty s
+--     pretty (Identifier s) = pretty s
+--     pretty (QfdIdentifier t c) = pretty t <> dot <> pretty c
+--     pretty Asterisk = prettyS "*"
+--     pretty (QfdAsterisk t) = pretty t <> dot <> prettyS "*"
+--     pretty (Neg e) = prettyS "-" <+> pretty e
+--     pretty (ArithBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
+--     pretty (Parens e) = parens $ pretty e
+--     pretty (CaseSt ce) = pretty ce
+--     pretty (Func f el) = pretty f <> (parens $ fillSep $ punctuate comma $ map pretty el) 
+--     pretty NullLit = prettyS "NULL"
+--     pretty (BoolLit b) = pretty b
+--     pretty (Not e) = prettyS "NOT" <+> pretty e
+--     pretty (Exists e) = prettyS "EXISTS" <+> pretty e
+--     pretty (BoolBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
+--     pretty (RelBinary op e1 e2) = fillSep [pretty e1, pretty op, pretty e2]
+
+
+-- instance Pretty CaseExpr where
+--     pretty (CaseExpr w e) = align $ vsep [prettyS "CASE",
+--                                           indent 4 $ vsep $ map wCl w,
+--                                           indent 4 $ eCl e,
+--                                           prettyS "END"]  
+
+--         where
+--             wCl (wp, we) = fillSep [prettyS "WHEN", pretty wp, prettyS "THEN", pretty we]
+--             eCl (Just expr) = prettyS "ELSE" <+> pretty expr
+--             eCl Nothing = prettyS ""
+
+-- instance Pretty ArithBinOp where
+--     pretty Add = prettyS "+"
+--     pretty Subtract = prettyS "-"
+--     pretty Multiply = prettyS "*"
+--     pretty Divide = prettyS "/"
+--     pretty Modulo = prettyS "%"
+
+-- instance Pretty RelnBinOp where
+--     pretty LessOrEq = prettyS "<="
+--     pretty GreaterOrEq = prettyS ">="
+--     pretty Less = prettyS "<"
+--     pretty Greater = prettyS ">" 
+--     pretty Like = prettyS "LIKE"
+--     pretty NotEq = prettyS "!="
+--     pretty Eq = prettyS "="
+
+-- instance Pretty BoolBinOp where
+--     pretty And = line <> prettyS "AND"
+--     pretty Or = line <> prettyS "OR"
+
+-- prettyS s = pretty (s :: String)
 
